f1371fd1c66548c38aac38416331564fff6c5515
Juliano Sena da Silva Carlos <julianossc@epesmalw045f.home>
2023-02-26 15:59:40 +0000

    Creating other solution for cluster

--- terraform/eks/versions.tf
+++ terraform/eks/versions.tf
@@ -1,19 +1,40 @@
 terraform {
+
+  cloud {
+    organization = "julianossc"
+    workspaces {
+      name = "infrastructure"
+    }
+  }
+
   required_version = ">= 1.0"
 
   required_providers {
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.47"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 3.0"
+      version = "~> 4.0.4"
     }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.10"
     }
+
     time = {
       source  = "hashicorp/time"
       version = ">= 0.9"
